37422781e2ee7bb77a0eada9f21e0a5de5039fd4
ChiuYeeJay <shes202@gmail.com>
2022-02-01 23:47:35 +0800

    toggle black

--- webextension/background.js
+++ webextension/background.js
@@ -1,8 +1,3 @@
-// var can_be_canceled = false;
-function cancel_shooting() {
-
-}
-
 // inject the content script to the current tab
 function go_shooting() {
     browser.tabs.executeScript({ file: "start.js" });
