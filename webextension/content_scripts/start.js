4e58754a171e4ea99d3cfcf303a67c0431000885
ChiuYeeJay <shes202@gmail.com>
2022-02-02 01:30:56 +0800

    modify layout and remove hint text

--- webextension/content_scripts/start.js
+++ webextension/content_scripts/start.js
@@ -17,11 +17,6 @@ function setup_start_html_elements() {
     var root = document.createElement("div");
     root.id = "qrshot_root_element";
 
-    let just_drag_txt = document.createElement("p");
-    just_drag_txt.id = "just_drag_txt";
-    just_drag_txt.innerText = "Just Drag";
-    root.appendChild(just_drag_txt);
-
     let cancel_btn = document.createElement("button");
     cancel_btn.id = "qrshot_cancel_btn";
     cancel_btn.classList.add("qrshot_btn");
